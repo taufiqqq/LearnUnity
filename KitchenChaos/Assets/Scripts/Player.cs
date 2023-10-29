f0c82e21d30588bc9a6bcb810ea8a0541063bd67
taufiqqq <muhdtaufiqbinjurimi@gmail.com>
2023-10-30 00:57:55 +0800

    detect collision

--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -13,9 +13,47 @@ public class Player : MonoBehaviour
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir  * moveSpeed * Time.deltaTime;
+        
         //use Time.deltaTime as unity will calculate using fps, higher fps will cause higher movement rate
         //so Time.deltaTime will tackle this issue and everyone will move at same rate
+        
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerSize = .7f;
+        float playerHeight = 2f;
+        
+        //bool canMove = !Physics.Raycast(transform.position, moveDir, playerSize);
+        //raycast, like laser but player visual cam boleh interpolate dengan wall
+
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerSize, moveDir, moveDistance);
+
+        if (!canMove)
+        {
+            //Cannot Move towards moveDir
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerSize, moveDirX, moveDistance);
+
+            if (canMove)
+            {
+                moveDir = moveDirX;
+            }else
+            {
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerSize, moveDirZ, moveDistance);
+                
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                    //Cannot MOVE ALL DIRECTION
+                }
+            }
+        }
+        if (canMove)
+        {
+             transform.position += moveDir  * moveDistance;
+        }
 
         isWalking = moveDir != Vector3.zero;
         
